acb51d97f78526efcbf4981d94bedbe17c226592
tin <tintran1209@gmail.com>
2023-12-26 16:24:56 -0800

    Added cameras data request from API service (missing files)

---
+++ Plugins/CivilFXCore/Source/Runtime/CivilFXCore/CommonCore/CivilFXCoreSettings.h
@@ -0,0 +1,22 @@
+// /** Copyright (C) CivilFX 2023 - All Rights Reserved */
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "CivilFXCoreSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Config = CivilFXCoreConfig, DefaultConfig, BlueprintType)
+class CIVILFXCORE_API UCivilFXCoreSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(config, EditAnywhere, Category = "API")
+	bool bUseAPI = false;
+	UPROPERTY(config, EditAnywhere, Category = "API", meta = (EditCondition = "bUseAPI"))
+	FString EndPoint;
+};
